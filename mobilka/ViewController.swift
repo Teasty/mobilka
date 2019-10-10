92df1527de3a693a7fec9ea8ec495635768752bc
Teasty <kostoprav28@mail.ru>
2019-10-10 13:48:56 +0300

    Initial Commit

---
+++ mobilka/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mobilka
+//
+//  Created by Андрей Лихачев on 10/10/2019.
+//  Copyright © 2019 Андрей Лихачев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
